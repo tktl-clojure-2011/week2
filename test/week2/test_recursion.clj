2d7b8d3f63eb9e0e1d52b15d6c4031a1b5b9d742
Juhana Laurinharju <fingerzam@gmail.com>
2011-02-01 18:26:37 +0200

    order doesn't matter in un-frequencies (Recursion)

--- test/week2/test_recursion.clj
+++ test/week2/test_recursion.clj
@@ -81,9 +81,9 @@
 
 (facts "un-frequencies"
        (un-frequencies {:a 3 :b 2 "^_^" 1})
-           => (:a :a :a "^_^" :b :b)
+           => (just [:a :a :a "^_^" :b :b] :in-any-order)
        (un-frequencies (my-frequencies [:a :b :c :a]))
-           => (:a :a :b :c)
+           => (just [:a :a :b :c] :in-any-order)
        (my-frequencies (un-frequencies {:a 100 :b 10}))
            => {:a 100 :b 10})
 
