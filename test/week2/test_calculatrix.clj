5b4d042a311722759ff847a779fe91972861e5ae
Ilmari Vacklin <ilmari.vacklin@cs.helsinki.fi>
2011-02-02 22:29:28 +0200

    add calculatrix facts

--- test/week2/test_calculatrix.clj
+++ test/week2/test_calculatrix.clj
@@ -4,11 +4,31 @@
   (:import (java.util.concurrent
              TimeoutException TimeUnit FutureTask)))
 
+(def *timeout* 200)
+
+(defn test-main [input]
+  (with-open [output (java.io.StringWriter.)]
+    (let [f (fn []
+              (binding [*out* output]
+                (with-in-str (str input "\n") (main))))
+          get-output (fn []
+                       (.trim
+                         (last (.split (str output)
+                                       "=>"))))
+          task (FutureTask. f)
+          thr (Thread. task)]
+      (try
+        (.start thr)
+        (.get task *timeout* TimeUnit/MILLISECONDS)
+        (get-output)
+        (catch TimeoutException e
+          (get-output))))))
+
 (facts "read-words"
-       (with-in-str "foo bar" (read-words)
-         => ["foo" "bar"])
-       (with-in-str "4 2   1" (read-words)
-         => ["4" "2" "1"]))
+       (with-in-str "foo bar" (read-words))
+         => ["foo" "bar"]
+       (with-in-str "4 2   1" (read-words))
+         => ["4" "2" "1"])
 
 (facts "string->number"
        (string->number "2")   => 2
@@ -22,23 +42,29 @@
        (compute "*" ["bar" "2"]) => nil
        (compute "foo" ["2" "2"]) => nil)
 
-(defn test-main [input]
-  (with-open [output (java.io.StringWriter.)]
-    (let [f (fn []
-              (binding [*out* output]
-                (with-in-str (str input "\n") (main))))
-          get-output (fn []
-                       (.trim 
-                         (second (.split (str output)
-                                         "=>"))))
-          task (FutureTask. f)
-          thr (Thread. task)]
-      (try
-        (.start thr)
-        (.get task 10 TimeUnit/MILLISECONDS)
-        (get-output)
-        (catch TimeoutException e
-          (get-output))))))
+(facts "Charmeleon"
+       (compute "-" ["2" "1"]) => 1
+       (test-main "- 32 3")    => "29")
+
+(facts "Miles Edgeworth"
+       (test-main "+ 2 foo")
+         => "Invalid operand: foo"
+       (test-main "foo 2 3")
+         => "Invalid command: foo")
+
+(facts "Manfred von Karma"
+       (test-main "+ 1 1\n* _ 2") => "4")
+
+(facts "The Milkman Conspiracy"
+       (test-main "avg 1 2") => "1.5"
+       (test-main "pow 2 3") => "8")
+
+(facts "Godot"
+       (test-main "store a 2\n* a 3") => "6")
+
+(facts "Bowser"
+       (test-main "+ 0 2 5 3") => "10"
+       (test-main "avg 1 2 3") => "2")
 
 (facts "main"
        (test-main "+ 2 2") => "4"
